6b520fb4d5c69b5a604790133392d817ff69bf53
Parker Johnson <mrstormthebeach@gmail.com>
2021-05-20 22:11:50 -0600

    move inputs to args

--- main.go
+++ main.go
@@ -1,23 +1,68 @@
+// 192 70 30 1.55
 package main
 
 import (
 	"fmt"
 	"math"
+	"os"
+	"strconv"
 )
 
+func roundToTwoSigFigs(i float64) float64 {
+	return math.Round(i*100) / 100
+}
+
 func main() {
-	var bodyweight float64 = 192
-	var heightInInches float64 = 70
+	/*
+		Until flags are implemented - you need to run program with args in location:
+		1: Bodyweight
+		2: Height (inches)
+		3. Age
+		4. Activity level (1-5)
+	*/
+	var err error
+	var bodyweight float64
+	var heightInInches float64
+	var age float64
+
+	var activityModifier float64
+	for idx, arg := range os.Args[1:] {
+		var activityModifierInput float64
+		switch idx {
+		case 0:
+			bodyweight, err = strconv.ParseFloat(arg, 64)
+		case 1:
+			heightInInches, err = strconv.ParseFloat(arg, 64)
+		case 2:
+			age, err = strconv.ParseFloat(arg, 64)
+		case 3:
+			activityModifierInput, err = strconv.ParseFloat(arg, 64)
+			if activityModifierInput == 1 {
+				// 1. Sedentary (little to no exercise + work a desk job) = 1.2
+				activityModifier = 1.2
+			} else if activityModifierInput == 2 {
+				// 2. Lightly Active (light exercise 1-3 days / week) = 1.375
+				activityModifier = 1.55
+			} else if activityModifierInput == 3 {
+				// 3. Moderately Active (moderate exercise 3-5 days / week) = 1.55
+				activityModifier = 1.55
+			} else if activityModifierInput == 4 {
+				// 4. Very Active (heavy exercise 6-7 days / week) = 1.725
+				activityModifier = 1.725
+			} else if activityModifierInput == 5 {
+				// 5. Extremely Active (very heavy exercise, hard labor job, training 2x / day) = 1.9
+				activityModifier = 1.9
+			}
+			activityModifier, err = strconv.ParseFloat(arg, 64)
+		}
+		if err != nil {
+			fmt.Println("Error", err)
+		}
+	}
 	var weightInKg = bodyweight / 2.2
 	var heightInCm = heightInInches * 2.54
-	var age float64 = 30
-	// Sedentary (little to no exercise + work a desk job) = 1.ve2
-	// Lightly Active (light exercise 1-3 days / week) = 1.375
-	// Moderately Active (moderate exercise 3-5 days / week) = 1.55
-	// Very Active (heavy exercise 6-7 days / week) = 1.725
-	// Extremely Active (very heavy exercise, hard labor job, training 2x / day) = 1.9
-	var activityModifier = 1.55
-	// Protein modifiers	:
+
+	// Protein modifiers:
 	// Cut: 1.4 - 1.1 g/lb
 	// Bulk: 0.8 - 1.0 g/lb
 	var proteinModifier = 1.2
@@ -29,11 +74,11 @@ func main() {
 	var BMR = 66 + (13.7 * weightInKg) + (1.8 * heightInCm) - (4.7 * age)
 	var TDEE = math.Round(activityModifier * BMR)
 
-	var proteinIntake = math.Round(bodyweight*proteinModifier*100) / 100
-	var fatIntake = math.Round(bodyweight*fatModifier*100) / 100
+	var proteinIntake = roundToTwoSigFigs(bodyweight * proteinModifier)
+	var fatIntake = roundToTwoSigFigs(bodyweight * fatModifier)
 	var calFromProtein = proteinIntake * 4
 	var calFromFat = fatIntake * 9
-	var calFromCarb = math.Round((TDEE-(calFromProtein+calFromFat))*100) / 100
+	var calFromCarb = roundToTwoSigFigs((TDEE - (calFromProtein + calFromFat)))
 	var carbIntake = calFromCarb / 4
 
 	fmt.Println("Daily intake")
