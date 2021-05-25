d23a1c86ded36998c49b233beaa93d9e695516ad
Parker Johnson <mrstormthebeach@gmail.com>
2021-05-25 07:09:10 -0600

    move from arg index parsing to using the flag package

--- main.go
+++ main.go
@@ -1,58 +1,13 @@
-// 192 70 30 1.55
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math"
-	"os"
-	"strconv"
 )
 
 func roundToTwoSigFigs(i float64) float64 {
 	return math.Round(i*100) / 100
-}
-
-func convertArgsToVars() (float64, float64, float64, float64) {
-	var err error
-	var bodyweight float64
-	var heightInInches float64
-	var age float64
-
-	var activityModifier float64
-	for idx, arg := range os.Args[1:] {
-		var activityModifierInput float64
-		switch idx {
-		case 0:
-			bodyweight, err = strconv.ParseFloat(arg, 64)
-		case 1:
-			heightInInches, err = strconv.ParseFloat(arg, 64)
-		case 2:
-			age, err = strconv.ParseFloat(arg, 64)
-		case 3:
-			activityModifierInput, err = strconv.ParseFloat(arg, 64)
-			if activityModifierInput == 1 {
-				// 1. Sedentary (little to no exercise + work a desk job) = 1.2
-				activityModifier = 1.2
-			} else if activityModifierInput == 2 {
-				// 2. Lightly Active (light exercise 1-3 days / week) = 1.375
-				activityModifier = 1.55
-			} else if activityModifierInput == 3 {
-				// 3. Moderately Active (moderate exercise 3-5 days / week) = 1.55
-				activityModifier = 1.55
-			} else if activityModifierInput == 4 {
-				// 4. Very Active (heavy exercise 6-7 days / week) = 1.725
-				activityModifier = 1.725
-			} else if activityModifierInput == 5 {
-				// 5. Extremely Active (very heavy exercise, hard labor job, training 2x / day) = 1.9
-				activityModifier = 1.9
-			}
-			activityModifier, err = strconv.ParseFloat(arg, 64)
-		}
-		if err != nil {
-			fmt.Println("Error", err)
-		}
-	}
-	return bodyweight, heightInInches, age, activityModifier
 }
 
 func printOutput(TDEE float64, proteinIntake float64, calFromProtein float64, carbIntake float64, calFromCarb float64, fatIntake float64, calFromFat float64) {
@@ -64,15 +19,42 @@ func printOutput(TDEE float64, proteinIntake float64, calFromProtein float64, ca
 	fmt.Println("Fat:", fatIntake, "g,", calFromFat, "cal")
 }
 
+func convertFlagsToVars() (float64, float64, float64, float64) {
+	bodyweight := flag.Int("bw", 0, "bodyweight in lbs")
+	height := flag.Int("h", 0, "height in inches")
+	age := flag.Int("age", 0, "age in years")
+	activity := flag.Int("activity", 0, "Activity Level Tier, where \n1 is Sedentary \n2 is Lightly Active \n3 is Moderately Active \n4 is Very Active \n5 is Extremely Active")
+
+	flag.Parse()
+
+	activityLevelModifier := convertActivityLevelTierToFunctionalValue(*activity)
+
+	return float64(*bodyweight), float64(*height), float64(*age), activityLevelModifier
+}
+
+func convertActivityLevelTierToFunctionalValue(activityLevelTier int) float64 {
+	var activityModifier float64
+	if activityLevelTier == 1 {
+		// 1. Sedentary (little to no exercise + work a desk job) = 1.2
+		activityModifier = 1.2
+	} else if activityLevelTier == 2 {
+		// 2. Lightly Active (light exercise 1-3 days / week) = 1.375
+		activityModifier = 1.55
+	} else if activityLevelTier == 3 {
+		// 3. Moderately Active (moderate exercise 3-5 days / week) = 1.55
+		activityModifier = 1.55
+	} else if activityLevelTier == 4 {
+		// 4. Very Active (heavy exercise 6-7 days / week) = 1.725
+		activityModifier = 1.725
+	} else if activityLevelTier == 5 {
+		// 5. Extremely Active (very heavy exercise, hard labor job, training 2x / day) = 1.9
+		activityModifier = 1.9
+	}
+	return activityModifier
+}
+
 func main() {
-	/*
-		Until flags are implemented - you need to run program with args in location:
-		1: Bodyweight
-		2: Height (inches)
-		3. Age
-		4. Activity level (1-5)
-	*/
-	var bodyweight, heightInInches, age, activityModifier = convertArgsToVars()
+	var bodyweight, heightInInches, age, activityModifier = convertFlagsToVars()
 
 	var weightInKg = bodyweight / 2.2
 	var heightInCm = heightInInches * 2.54
--- main_test.go
+++ main_test.go
@@ -15,25 +15,3 @@ func TestRoundTwoSigFigs(t *testing.T) {
 		}
 	})
 }
-
-func TestConvertArgsToVars(t *testing.T) {
-	t.Run("Converts args to the vars we need", func(t *testing.T) {
-		oldArgs := os.Args
-		defer func() { os.Args = oldArgs }()
-		os.Args = []string{"cmd", "192", "70", "30", "3"}
-		var got1, got2, got3, got4 = convertArgsToVars()
-		var want1, want2, want3, want4 float64 = 192, 70, 30, 3
-		if got1 != want1 {
-			t.Error("Failed Test, 1", got1, want1)
-		}
-		if got2 != want2 {
-			t.Error("Failed Test, 2", got2, want2)
-		}
-		if got3 != want3 {
-			t.Error("Failed Test, 3", got3, want3)
-		}
-		if got4 != want4 {
-			t.Error("Failed Test, 4", got4, want4)
-		}
-	})
-}
